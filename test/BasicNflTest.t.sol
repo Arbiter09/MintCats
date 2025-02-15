1d8d3ab565318a4131ce622b85ad6a1bcf576f77
Arbiter09 <jasdhiren.shah@stonybrook.edu>
2025-02-14 23:23:03 -0500

    Initial commit: MintCats NFT project

---
+++ test/BasicNflTest.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant CAT =
+        "https://bafybeif22fpzuu4wncff4jtx7jnvaqrr5ugs73szsbk2iknmq2ramtbfsq.ipfs.dweb.link?filename=cat.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Cats_<3";
+        string memory actualName = basicNft.name();
+
+        assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(CAT);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(CAT)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
+    }
+}
